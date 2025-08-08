190d42b100a3431103c348587d1ec1156d6ba6a2
MS10 CWWeng <cwweng@nuvoton.com>
2025-08-08 16:05:02 +0800

    Add D0/H0 QSPI/SPI/I2S driver and update Winbond SPI NAND driver
    Change-Id: If54b81f49c7e641665712ed29d80d381e4fc6ec0

---
+++ sound/soc/ma35h0/ma35h0-pcm.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2025, Nuvoton Corporation.  All rights reserved.
+ */
+
+#ifndef __MA35H0_PCM_H__
+#define __MA35H0_PCM_H__
+
+int devm_ma35h0_pcm_platform_register(struct device *dev);
+
+#endif
