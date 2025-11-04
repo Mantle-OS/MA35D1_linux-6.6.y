e208027ab8d173e47c6a2f7f91aaa56f9f093674
MS10 CWWeng <cwweng@nuvoton.com>
2025-11-04 16:09:02 +0800

    Update MTD SPI NOR driver
    Add following SPI NOR Flash support
    1. ISSI IS25LP512M
    2. Winbond w25q01nw/w25q01jv/w25q512jv
    
    Change-Id: Ibe856feb6d82754949fe1e48dffff21a6f17c7b1

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -127,6 +127,9 @@ static const struct flash_info winbond_nor_parts[] = {
 	{ "w25q256", INFO(0xef4019, 0, 64 * 1024, 512)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
 		.fixups = &w25q256_fixups },
+	{ "w25q512", INFO(0xef4020, 0, 64 * 1024, 1024)
+		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
+		.fixups = &w25q256_fixups },
 	{ "w25q256jvm", INFO(0xef7019, 0, 64 * 1024, 512)
 		PARSE_SFDP },
 	{ "w25q256jw", INFO(0xef6019, 0, 64 * 1024, 512)
@@ -138,10 +141,16 @@ static const struct flash_info winbond_nor_parts[] = {
 	{ "w25q512nwq", INFO(0xef6020, 0, 0, 0)
 		PARSE_SFDP
 		OTP_INFO(256, 3, 0x1000, 0x1000) },
+	{ "w25q01nw", INFO(0xef6021, 0, 0, 0)
+		PARSE_SFDP
+		OTP_INFO(256, 3, 0x1000, 0x1000) },
 	{ "w25q512nwm", INFO(0xef8020, 0, 64 * 1024, 1024)
 		PARSE_SFDP
 		OTP_INFO(256, 3, 0x1000, 0x1000) },
 	{ "w25q512jvq", INFO(0xef4020, 0, 64 * 1024, 1024)
+		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ |
+			      SPI_NOR_QUAD_READ) },
+	{ "w25q01jv", INFO(0xef4021, 0, 64 * 1024, 2048)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ |
 			      SPI_NOR_QUAD_READ) },
 };
