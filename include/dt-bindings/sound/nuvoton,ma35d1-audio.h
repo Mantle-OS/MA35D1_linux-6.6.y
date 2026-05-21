1272b3d2278dc6ae32bd89cf95d5b2ebd9c9f366
jmills <josephjamesmills@gmail.com>
2026-05-21 01:40:42 -0500

    simple update to dts

---
+++ include/dt-bindings/sound/nuvoton,ma35d1-audio.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0+ OR MIT */
+/*
+ * Copyright (c) 2026
+ */
+
+#ifndef __DT_BINDINGS_MA35D1_AUDIO_H
+#define __DT_BINDINGS_MA35D1_AUDIO_H
+
+
+#define MA35D1_I2S0_TX_DMA_REQSEL  86
+#define MA35D1_I2S0_RX_DMA_REQSEL  87
+
+#define MA35D1_I2S1_TX_DMA_REQSEL  88
+#define MA35D1_I2S1_RX_DMA_REQSEL  89
+
+
+
+#endif
